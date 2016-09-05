8da192f9d157c0cffda6b82c91d575fd6450d2ad
Serhii Bozhko <serhii.bozhko@gmail.com>
2016-09-06 00:10:15 +0300

    New file structure

---
+++ 03_ReactJS/webpack.config.js
@@ -0,0 +1,25 @@
+﻿/// <binding ProjectOpened='Watch - Development' /> 
+"use strict";
+
+module.exports = {
+    entry: {
+        "Lesson_19/CW/build/bundle_2": "./Lesson_19/CW/jsx/2.jsx",
+        "Lesson_19/CW/build/bundle_3": "./Lesson_19/CW/jsx/3.jsx"
+    } ,
+           
+    output: {
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                query:
+                {
+                       presets: ['react']
+                }
+            }
+        ]
+    }
+};
