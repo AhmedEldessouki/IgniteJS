4de07b0e24e2cf7840ccf6e03a6fb1a8c1851759
Serhii Bozhko <serhii.bozhko@gmail.com>
2016-09-06 23:14:41 +0300

    Lesson 19 HW

--- 03_ReactJS/webpack.config.js
+++ 03_ReactJS/webpack.config.js
@@ -3,8 +3,17 @@
 
 module.exports = {
     entry: {
+
+        // Lesson 19
         "Lesson_19/CW/build/bundle_2": "./Lesson_19/CW/jsx/2.jsx",
-        "Lesson_19/CW/build/bundle_3": "./Lesson_19/CW/jsx/3.jsx"
+        "Lesson_19/CW/build/bundle_3": "./Lesson_19/CW/jsx/3.jsx",
+
+        "Lesson_19/HW/build/bundle_1": "./Lesson_19/HW/jsx/1.jsx",
+        "Lesson_19/HW/build/bundle_2": "./Lesson_19/HW/jsx/2.jsx",
+        "Lesson_19/HW/build/bundle_3": "./Lesson_19/HW/jsx/3.jsx"
+
+
+        // Lesson 20
     } ,
            
     output: {
